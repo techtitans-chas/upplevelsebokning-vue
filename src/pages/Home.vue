6a2977c9f7692451ad16a39a7cc6093b45468911
Martinsodersten <martin_sodersten@hotmail.com>
2025-11-25 12:02:20 +0100

    Added data for accommodation and destinations

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,22 +1,24 @@
 <template>
   <Section>
     <h1>Welcome to Hourglass Adventures!</h1>
-    <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. In deleniti voluptas, dolorum perferendis
-      hic harum ex, inventore blanditiis necessitatibus fugit iusto praesentium minus optio maiores quam magni
-      voluptates dignissimos repellat?</p>
+    <p class="mb-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. In deleniti
+      voluptas, dolorum perferendis hic harum ex, inventore blanditiis
+      necessitatibus fugit iusto praesentium minus optio maiores quam magni
+      voluptates dignissimos repellat?
+    </p>
 
     <h2>Search</h2>
 
     <SearchForm class="mb-6" />
 
-    <Button to="/search" icon-left="iconamoon:search-bold" icon="fluent:arrow-right-12-filled">
-      Search
-    </Button>
-
-    <h2>Destinations</h2>
+    <h2>Current offers</h2>
     <div class="grid grid-cols-[1fr_1fr_1fr] gap-4 p-8">
       <div v-for="item in destinations.data" :key="item.id">
-        <RouterLink :to="`/destination/${item.id}`" class="border border-1 p-4 block">
+        <RouterLink
+          :to="`/destination/${item.id}`"
+          class="border border-1 p-4 block"
+        >
           {{ item.title }}
         </RouterLink>
       </div>
@@ -24,22 +26,18 @@
   </Section>
 
   <Section>
-    <Card>
-      Testing
-    </Card>
+    <Card> Testing </Card>
   </Section>
 </template>
 
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
-import Button from "@/components/Button.vue";
 import SearchForm from "@/components/SearchForm.vue";
 import Section from "@/components/Section.vue";
 import Card from "@/components/Card.vue";
 import { useDestinationStore } from "@/stores/destination";
 
 const destinations = useDestinationStore();
-
 </script>
 
 <style scoped>
