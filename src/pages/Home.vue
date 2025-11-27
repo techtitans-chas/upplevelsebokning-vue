42e377022fb2fc3d72cde2f137a4d004a98260c0
Ida Öhlén <ida-alexandra.ohlen@chasacademy.se>
2025-11-28 00:27:07 +0100

    Style home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,35 +1,36 @@
 <template>
-  <Section
-    bg-image="/images/placeholder.jpg"
-    bg-color="bg-primary-950/70"
-    class="bg-fixed"
-  >
-    <h1>Welcome to Hourglass Adventures!</h1>
-    <p class="mb-4">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. In deleniti
-      voluptas, dolorum perferendis hic harum ex, inventore blanditiis
-      necessitatibus fugit iusto praesentium minus optio maiores quam magni
-      voluptates dignissimos repellat?
-    </p>
-
-    <Modal title="My modal" :actions="actionButtons">
+  <Section center bg-image="/images/placeholder.jpg" bg-color="bg-primary-950/70" class="bg-fixed min-h-[500px]" inner-class="w-full">
+    <h1 class="leading-snug mb-6"><span class="text-white">Welcome to</span><br /> Hourglass Adventures</h1>
+    <p class="text-xl mb-8 max-w-xl">Embark on extraordinary journeys through time. Explore ancient civilizations,
+      witness pivotal moments in history, and experience unforgettable nights in the heart of bygone eras.</p>
+    <Button color="secondary" size="lg" to="/search" icon="streamline-plump:hourglass-remix">Browse our destinations</Button>
+  </Section>
+  <Section class="p-6">
+    <!-- <Modal title="My modal" :actions="actionButtons">
       Here's a modal!
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-    </Modal>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    </Modal> -->
 
-    <h2>Search</h2>
+    <h2 class="sr-only">Search</h2>
 
-    <SearchForm class="mb-6" />
-
-    <h2>Featured destinations</h2>
+    <SearchForm />
+  </Section>
+  <Section bg-image="/images/placeholder-2.jpg" bg-color="bg-primary-950/70" class="p-8">
+    <h2 class="text-center">Featured destinations</h2>
     <div class="grid grid-cols-[1fr_1fr_1fr] gap-4">
-      <DestinationCard
-        v-for="item in destinations.data"
-        :key="item.id"
-        :data="item"
-        @click="router.push(`/destination/${item.id}`)"
-      />
+      <DestinationCard v-for="item in destinations.data" :key="item.id" :data="item"
+        @click="router.push(`/destination/${item.id}`)" />
     </div>
+  </Section>
+  <Section class="p-12 text-center">
+    <h2>Step beyond the boundaries of time itself.</h2>
+    <p class="mb-4">
+      Hourglass Adventures offers extraordinary journeys through history,
+      allowing you to experience iconic moments and immerse yourself in distant eras. Whether you dream of walking
+      through ancient civilizations, witnessing pivotal historical events, or exploring the customs of a bygone age, we
+      make the impossible possible. Book your time travel expedition today and spend unforgettable nights in the heart
+      of history.
+    </p>
   </Section>
 </template>
 
@@ -39,7 +40,7 @@ import SearchForm from "@/components/form/SearchForm.vue";
 import Section from "@/components/layout/Section.vue";
 import DestinationCard from "@/components/cards/DestinationCard.vue";
 import { useDestinationStore } from "@/stores/destination";
-import Modal from "@/components/ui/Modal.vue";
+import Button from "@/components/ui/Button.vue";
 
 const router = useRouter();
 
@@ -50,10 +51,3 @@ const actionButtons = [
   { title: "Accept", action: () => console.log("Accept") },
 ];
 </script>
-
-<style scoped>
-.box {
-  height: 100px;
-  width: 100%;
-}
-</style>
