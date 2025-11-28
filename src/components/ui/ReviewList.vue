943f8bf027acfd1fe9b3aa00fdf699f3a7c2ce16
Ida Öhlén <ida-alexandra.ohlen@chasacademy.se>
2025-11-28 10:00:25 +0100

    Add Review List component

---
+++ src/components/ui/ReviewList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="review in data" :key="review.user"
+    class="grid grid-rows-subgrid row-span-3 text-center max-sm:not-last:mb-6">
+    <span class="font-semibold">
+      {{ review.user }}
+    </span>
+    <div class="flex justify-center">
+      <Icon icon="material-symbols:star-rounded" v-for="stars in review.rating" :key="stars"
+        class="text-secondary-300 text-3xl" />
+      <Icon icon="material-symbols:star-rounded" v-for="stars in (5 - review.rating)" :key="stars"
+        class="text-primary-700 text-3xl" />
+    </div>
+    {{ review.comment }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+import type { Review } from "@/types";
+
+defineProps<{
+  data: Review[]
+}>();
+</script>
--- src/pages/Destination.vue
+++ src/pages/Destination.vue
@@ -1,29 +1,39 @@
 <template>
-  <!-- Destination -->
-  <Section
-    v-if="destination"
-    center
-    class="min-h-[400px]"
-    bg-color="bg-black/60"
-    :bg-image="
-      destination.coverImage
+  <div v-if="destination">
+    <Section center class="min-h-[400px]" bg-color="bg-black/60" :bg-image="destination.coverImage
         ? destination.coverImage
         : '/images/placeholder.jpg'
-    "
-  >
-    <div>
-      <div class="flex items-center gap-4">
-        <h1>{{ destination.title }}</h1>
-        <Badge>{{ destination.timePeriod }}</Badge>
+      ">
+      <div>
+        <div class="flex items-center gap-4">
+          <h1>{{ destination.title }}</h1>
+          <div>
+            <div class="text-xs">Time period</div>
+            <Badge>{{ destination.timePeriod }}</Badge>
+          </div>
+        </div>
+        <div class="grid gap-2 p-6 items-start">
+          <div v-if="destination.description" class="text-white">
+            {{ destination.description }}
+          </div>
+          <div v-if="destination.climate" class="text-primary-200 w-fit">
+            <Badge class="bg-primary-950"><Icon icon="tabler:sun" />{{ destination.climate }}</Badge>
+          </div>
+        </div>
+        <Button :to="`/booking/${destination.id}`" icon="fa7-solid:arrow-right">
+          Book your journey
+        </Button>
       </div>
-      <p v-if="destination.description" class="text-white">
-        {{ destination.description }}
-      </p>
-      <Button :to="`/booking/${destination.id}`" icon="fa7-solid:arrow-right"
-        >Book Now</Button
-      >
-    </div>
-  </Section>
+    </Section>
+    <!-- Reviews -->
+    <Section class="p-10 pt-6">
+      <h2 class="text-center mb-8">Reviews</h2>
+
+      <div class="grid grid-cols-1 sm:grid-cols-2 grid-rows-[auto_auto_auto] sm:gap-x-4 sm:gap-y-2 max-w-lg mx-auto px-2">
+        <ReviewList :data="destination.reviews" />
+      </div>
+    </Section>
+  </div>
   <Section v-else>
     <h1>Destination not found</h1>
   </Section>
@@ -36,6 +46,8 @@ import { useDestinationStore } from "@/stores/destination";
 import Section from "@/components/layout/Section.vue";
 import Button from "@/components/ui/Button.vue";
 import Badge from "@/components/ui/Badge.vue";
+import ReviewList from "@/components/ui/ReviewList.vue";
+import { Icon } from "@iconify/vue";
 
 const route = useRoute();
 const destinationStore = useDestinationStore();
